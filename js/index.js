bac68ccc5fa1a858bbef165cb84f1e7b32b0ecd9
Holden Altaffer <haltaffer@gmail.com>
2019-01-18 02:17:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 // your code here
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `${commits
+  const commitsList = `<ul>${commits
     .map(
       commit =>
-        // '<li><strong>' +
-        // commit.author.login +
-        // '</strong> - ' +
-        // commit.commit.message +
-        // '</li>'
-        '/' + commit.author.login + '/'
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
     )
-    .join('')}`;
+    .join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
 }
