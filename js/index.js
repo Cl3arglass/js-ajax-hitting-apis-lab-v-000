0b9e36e52265b5748459e3391606092e677138b5
Holden Altaffer <haltaffer@gmail.com>
2019-01-18 02:59:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -66,10 +66,10 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
-// function getCommits(el) {
-//   const name = el.dataset.repo;
-//   const req = new XMLHttpRequest();
-//   req.addEventListener('load', showCommits);
-//   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
-//   req.send();
-// }
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
